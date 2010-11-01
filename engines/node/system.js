17aa6635f219158915c740446e0008f8dcd67a6e
Irakli Gozalishvili <rfobic@gmail.com>
2010-11-01 03:19:16 +0100

    Moving node adapter for system module to the 'engines/node'.

--- engines/node/system.js
+++ engines/node/system.js
@@ -1,10 +1,15 @@
 'use strict'
 
-var stdin
+var sys = require('sys')
+
+,   stdin
+
 module.exports = Object.freeze(Object.create(Object.prototype,
 { stdout: { value: process.stdout }
 , stdin: { get: function() { return stdin || (stdin = process.openStdin()) } }
 , stderr: { get: function() { return stdin || (stdin = process.openStdin()) } }
 , env: { value: process.env }
 , args: { value: process.argv }
+, print: { value: sys.puts }
+, engine: { value: 'node' }
 }))
