6e0f473831eb288e195d40865a454cf8c4f35e93
Irakli Gozalishvili <rfobic@gmail.com>
2010-11-01 11:41:15 +0100

    Trying to use `util` module first to get read of the warning in the console on node >= 0.3.0

--- engines/node/system.js
+++ engines/node/system.js
@@ -1,8 +1,13 @@
 'use strict'
 
-var sys = require('sys')
-
+var print
 ,   stdin
+
+try { // node >= 0.3.0
+  print = require('util').puts
+} catch (e) { // node <= 0.3.0
+  print = require('sys').puts
+}
 
 module.exports = Object.freeze(Object.create(Object.prototype,
 { stdout: { value: process.stdout }
@@ -10,6 +15,6 @@ module.exports = Object.freeze(Object.create(Object.prototype,
 , stderr: { get: function() { return stdin || (stdin = process.openStdin()) } }
 , env: { value: process.env }
 , args: { value: process.argv }
-, print: { value: sys.puts }
+, print: { value: print }
 , engine: { value: 'node' }
 }))
