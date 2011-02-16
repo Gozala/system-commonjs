389e852b542da69dfe20987f6d00758d9dac94ee
Irakli Gozalishvili <rfobic@gmail.com>
2011-02-17 00:48:40 +0100

    Update to node 0.4.0

--- engines/node/system.js
+++ engines/node/system.js
@@ -1,7 +1,6 @@
 'use strict'
 
-var print
-,   stdin
+var print, stdin
 
 try { // node >= 0.3.0
   print = require('util').puts
@@ -9,12 +8,37 @@ try { // node >= 0.3.0
   print = require('sys').puts
 }
 
-module.exports = Object.freeze(Object.create(Object.prototype,
-{ stdout: { value: process.stdout }
-, stdin: { get: function() { return stdin || (stdin = process.openStdin()) } }
-, stderr: { get: function() { return stdin || (stdin = process.openStdin()) } }
-, env: { value: process.env }
-, args: { value: process.argv }
-, print: { value: print }
-, engine: { value: 'node' }
-}))
+Object.defineProperties(exports, {
+  stdout: {
+    value: process.stdout,
+    enumerable: true
+  },
+  stdin: {
+    get: function() {
+      return stdin || (stdin = process.openStdin())
+    },
+    enumerable: true
+  },
+  stderr: {
+    get: function() {
+      return stdin || (stdin = process.openStdin())
+    },
+    enumerable: true
+  },
+  env: {
+    value: process.env,
+    enumerable: true
+  },
+  args: {
+    value: process.argv,
+    enumerable: true
+  },
+  print: {
+    value: print,
+    enumerable: true
+  },
+  engine: {
+    value: 'node',
+    enumerable: true
+  }
+})
